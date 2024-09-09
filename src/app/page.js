d0a1352f49ac369d164427d18866a8b0de33c6d2
Mateus Collet Lacerda Machado <mateuscolletmac@gmail.com>
2024-09-09 13:33:15 -0300

    jpg add

--- src/app/page.js
+++ src/app/page.js
@@ -2,36 +2,36 @@
 import React, { useState } from 'react'
 import './globals.css'
 
-export default function Home(){
+export default function Home() {
 
   const [numero, setNumero] = useState(0)
 
   return (
     <section className='geral'>
       <div id='topo'>
-    <h1>COUNTER</h1>
-    <p>{numero}</p>
-    </div>
-    <section className='btns'>
-    <section className='btn-p'>
-    <button className='positivos' onClick={aumentar1}>+1</button>
-    <button className='positivos' onClick={aumentar10}>+10</button>
-    <button className='positivos' onClick={aumentar100}>+100</button>
-    <button className='positivos' onClick={aumentar1000}>+1000</button>
-    <button className='positivos' onClick={aumentar10000}>+10000</button>
-    </section>
-    <button className='z-btn' onClick={zerar}>ZERAR</button>
-    <section className='btn-n'>
-    <button className='negativos' onClick={diminuir1}>-1</button>
-    <button className='negativos' onClick={diminuir10}>-10</button>
-    <button className='negativos' onClick={diminuir100}>-100</button>
-    <button className='negativos' onClick={diminuir1000}>-1000</button>
-    <button className='negativos' onClick={diminuir10000}>-10000</button>
-    </section>
-    </section>
+        <h1>COUNTER</h1>
+        <p>{numero}</p>
+      </div>
+      <section className='btns'>
+        <section className='btn-p'>
+          <button className='positivos' onClick={aumentar1}>+1</button>
+          <button className='positivos' onClick={aumentar10}>+10</button>
+          <button className='positivos' onClick={aumentar100}>+100</button>
+          <button className='positivos' onClick={aumentar1000}>+1000</button>
+          <button className='positivos' onClick={aumentar10000}>+10000</button>
+        </section>
+        <button className='z-btn' onClick={zerar}>ZERAR</button>
+        <section className='btn-n'>
+          <button className='negativos' onClick={diminuir1}>-1</button>
+          <button className='negativos' onClick={diminuir10}>-10</button>
+          <button className='negativos' onClick={diminuir100}>-100</button>
+          <button className='negativos' onClick={diminuir1000}>-1000</button>
+          <button className='negativos' onClick={diminuir10000}>-10000</button>
+        </section>
+      </section>
     </section>
   );
-  
+
   function aumentar1() {
     setNumero(numero + 1)
   }
